e62218f9d9a45901983ef98b95d57ce31c8babcb
Dave Penn <djpenn3@pennsive.com>
2020-04-15 01:01:51 -0500

    app set-up

---
+++ src/main/java/com/pennsive/myretail/MyRetailApplication.java
@@ -0,0 +1,15 @@
+package com.pennsive.myretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class MyRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetailApplication.class, args);
+	}
+
+}
