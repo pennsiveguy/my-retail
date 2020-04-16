cd68d09e0c53f89098ed8a17df9e438059aef5ef
Dave Penn <djpenn3@pennsive.com>
2020-04-16 04:26:07 -0500

    Swagger refinements

--- src/main/java/com/pennsive/myretail/controller/ProductController.java
+++ src/main/java/com/pennsive/myretail/controller/ProductController.java
@@ -1,4 +1,5 @@
 package com.pennsive.myretail.controller;
+import org.apache.commons.lang3.ObjectUtils.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,13 +7,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pennsive.myretail.aggregator.ProductAggregatorService;
 import com.pennsive.myretail.response.Price;
 import com.pennsive.myretail.response.Product;
 import com.pennsive.myretail.service.PriceDocumentService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping(path="/products")
@@ -24,13 +30,26 @@ public class ProductController {
 	private PriceDocumentService priceDocumentService;
 	
 	@GetMapping(path = "/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public Product getProduct(@PathVariable("id") Long id) {
+	@Operation(description = "Find a product by its ID.",
+	responses = {
+			@ApiResponse(content = @Content(schema = @Schema(implementation = Product.class)), responseCode = "200"),
+			@ApiResponse(content = @Content(schema = @Schema(implementation = Null.class)), responseCode = "404", description = "No product exists with that ID."),
+			@ApiResponse(content = @Content(schema = @Schema(implementation = Null.class)), responseCode = "400", description = "The product ID must be a number.")
+	})
+	public Product getProduct(@Parameter(description = "The product ID you wish to look up", required = true) @PathVariable("id") Long id) {
 		return productAggregator.getProduct(id);
 	}
 	
 	@PostMapping(path = "/{id}")
-	public void updatePrice(@RequestBody Price price, @PathVariable("id") Long id) {
+	@Operation(description = "Update an item's price by its ID.",
+	responses = {
+			@ApiResponse(content = @Content(schema = @Schema(implementation = Product.class)), responseCode = "200"),
+			@ApiResponse(content = @Content(schema = @Schema(implementation = Null.class)), responseCode = "404", description = "No product exists with that ID."),
+			@ApiResponse(content = @Content(schema = @Schema(implementation = Null.class)), responseCode = "400", description = "The product ID must be a number.")
+	})
+	public void updatePrice(
+			@Parameter(description = "The new price", required = true) @RequestBody Price price,
+			@Parameter(description = "The product ID whose price you wish to update", required = true) @PathVariable("id") Long id) {
 		priceDocumentService.updatePrice(id, price);
 	}
 }
