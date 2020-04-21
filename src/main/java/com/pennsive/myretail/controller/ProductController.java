28b5bb727299095735f22413c6b5ed4dc7b87714
Dave Penn <djpenn3@pennsive.com>
2020-04-21 02:18:43 -0500

    Misc. clean-up

--- src/main/java/com/pennsive/myretail/controller/ProductController.java
+++ src/main/java/com/pennsive/myretail/controller/ProductController.java
@@ -32,31 +32,34 @@ public class ProductController {
 	
 	@GetMapping(path = "/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
 	@Operation(description = "Find a product by its numeric ID. "
-			+ "<p>Some IDs which return results are 54191101, 53727884, and 13860428. </p>"
-			+ "<p>Non-numeric IDs (i.e. 'foobar') will be flagged as bad requests.</p>"
-			+ "<p>Failure to submit an ID will result in a 404 - Not Found"
-			+ "</p>",
+		+ "<p>Some IDs which return results are 54191101, 53727884, and 13860428. </p>"
+		+ "<p>Non-numeric IDs (i.e. 'foobar') will be flagged as bad requests.</p>"
+		+ "<p>Failure to submit an ID will result in a 404 - Not Found"
+		+ "</p>",
 	responses = {
-			@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ProductResponse.class))),
-			@ApiResponse(responseCode = "404", content = @Content(schema = @Schema()),
-				description = "No product exists with that ID. Try <a href='https://amazon.com'>Amazon</a>. KIDDING!!!"),
-			@ApiResponse(responseCode = "400", content = @Content(schema = @Schema()),
-				description = "The product ID must be a number.")
+		@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ProductResponse.class)),
+			description = "Request successful"),
+		@ApiResponse(responseCode = "404", content = @Content(schema = @Schema()),
+			description = "No product exists with that ID. Try <a href='https://amazon.com'>Amazon</a>. KIDDING!!!"),
+		@ApiResponse(responseCode = "400", content = @Content(schema = @Schema()),
+			description = "The product ID must be a number.")
 	})
-	public ProductResponse getProduct(@Parameter(description = "The product ID you wish to look up", required = true) @PathVariable("id") Integer id) {
+	public ProductResponse getProduct(
+		@Parameter(description = "The ID of the product whose data you wish to retrieve", required = true) @PathVariable("id") Integer id) {
 		return productAggregator.getProduct(id);
 	}
 	
 	@PutMapping(path = "/{id}", consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	@Operation(description = "Update an item's price. Pick one of the IDs above.",
 	responses = {
-			@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = PriceResponse.class))),
-			@ApiResponse(responseCode = "404", description = "No product exists with that ID."),
-			@ApiResponse(responseCode = "400", description = "The product ID must be a number.")
+		@ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = PriceResponse.class)),
+			description = "Update successful"),
+		@ApiResponse(responseCode = "404", description = "No product exists with that ID."),
+		@ApiResponse(responseCode = "400", description = "The product ID must be a number.")
 	})
 	public void updatePrice(
-			@Parameter(description = "The new price", required = true) @RequestBody PriceResponse price,
-			@Parameter(description = "The product ID whose price you wish to update", required = true) @PathVariable("id") Integer id) {
+		@Parameter(description = "The new price", required = true) @RequestBody PriceResponse price,
+		@Parameter(description = "The ID of the product whose price you wish to update", required = true) @PathVariable("id") Integer id) {
 		priceDocumentService.updatePrice(id, price);
 	}
 }
