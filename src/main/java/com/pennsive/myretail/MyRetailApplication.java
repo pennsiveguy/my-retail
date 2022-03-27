39105b17c2c7ed9a73be6cf141aa85fc178b188e
dpenn <dpenn@vecna.com>
2022-03-27 01:04:08 -0500

    Updating Spring versions

--- src/main/java/com/pennsive/myretail/MyRetailApplication.java
+++ src/main/java/com/pennsive/myretail/MyRetailApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 @EnableAsync
 public class MyRetailApplication {
@@ -11,5 +16,4 @@ public class MyRetailApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyRetailApplication.class, args);
 	}
-
 }
